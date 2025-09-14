974ce2cd38c10060efc14cdf1e034f12b325910e
Inszert <danielszabo.main@gmail.com>
2025-09-14 23:53:01 +0200

    d

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
+import os
 from flask import Flask
-from models import db
-from routes import init_routes
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://<username>:<password>@<host>/<dbname>"
+
+# Získame URL databázy z environment premennej
+database_url = os.environ.get("DATABASE_URL")
+if database_url.startswith("postgres://"):
+    database_url = database_url.replace("postgres://", "postgresql://", 1)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = database_url
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db.init_app(app)
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "fallback_secret")
 
-with app.app_context():
-    db.create_all()
+# Inicializácia SQLAlchemy
+db = SQLAlchemy(app)
 
-init_routes(app)
+# Import routov a modelov
+from routes import *
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(host="0.0.0.0", port=8000)
