f160a90234ad91ae45f50954f6c6298e2c64bd52
Inszert <danielszabo.main@gmail.com>
2025-09-14 23:14:37 +0200

    d

--- app.py
+++ app.py
@@ -7,13 +7,14 @@ app = Flask(__name__)
 # -------------------- KONFIGURÁCIA --------------------
 database_url = os.environ['DATABASE_URL']
 
-# Ak URL začína "postgres://", nahraď ju správnym prefixom pre psycopg 3
+database_url = os.environ['DATABASE_URL']
+
+# Ak URL začína "postgres://", nahraď "postgresql+psycopg://" pre SQLAlchemy s psycopg3
 if database_url.startswith("postgres://"):
     database_url = database_url.replace("postgres://", "postgresql+psycopg://", 1)
-elif database_url.startswith("postgresql://"):
-    database_url = database_url.replace("postgresql://", "postgresql+psycopg://", 1)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = database_url
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Tajný kľúč pre Flask sessions
